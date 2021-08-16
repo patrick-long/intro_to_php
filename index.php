98ad1a17b19fd6d71faa6d0cc3d881c0110872f6
longpatrick <long.patrick1997@gmail.com>
2021-08-16 12:52:30 -0500

    start working with numbers in php

--- index.php
+++ index.php
@@ -12,9 +12,20 @@
         echo("<h1>$siteOwner's site</h1>");
         echo "<hr>";
         echo "<p>This is my site</p>";
+
+        echo "<h4>Working with string modifications</h4>";
         echo strtolower($siteOwner);
         echo "<br>";
         echo strtoupper($siteOwner);
+        echo "<br>";
+        echo $siteOwner[0];
+        echo "<br>";
+        echo str_replace('Patrick', 'Abigayle', $siteOwner);
+        echo "<br>";
+        echo substr($siteOwner, 0, 3);
+        echo "<br>";
+        echo "<br>";
+
 
         // Working with data types in php
         // Strings
@@ -31,6 +42,18 @@
 
         // Null
         $nothingness = null;
+
+        // Working with numbers in php
+        echo -47;
+        echo "<br>";
+        echo 5+9;
+        echo "<br>";
+        echo 5.79 * 31.9;
+        echo "<br>";
+        echo 10 % 3;
+        echo "<br>";
+        echo "<br>";
+
 
         $siteOwner = 'Not Patrick';
         echo "<footer>This is now $siteOwner's site."
