31b19b90db9968306db0119c713833ac35d8691f
longpatrick <long.patrick1997@gmail.com>
2021-08-18 10:24:15 -0500

    refactor getMax function into a 3 parameter function

--- index.php
+++ index.php
@@ -95,15 +95,17 @@
 
 
         // Work with other functions in php using if statements
-        function getMax($num1, $num2) {
-            if ($num1 > $num2) {
+        function getMax($num1, $num2, $num3) {
+            if ($num1 >= $num2 && $num1 >= $num3) {
                 return $num1;
-            } else {
+            } elseif ($num2 >= $num1 && $num2 >= $num3) {
                 return $num2;
+            } else {
+                return $num3;
             };
         };
 
-        echo getMax(10, 2);
+        echo getMax(10000, 200, 109);
     ?>
 
 
