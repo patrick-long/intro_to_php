3a012a5a656beb9893394550b3941dd5835b8b23
longpatrick <long.patrick1997@gmail.com>
2021-08-18 11:12:57 -0500

    include header.html and footer.html in php file for code reuse

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Intro</title>
 </head>
 <body>
 
@@ -179,66 +179,73 @@
 
     <!-- Working with for loops in php -->
     <?php
-        $luckyNumbers = array(4, 8, 15, 16, 23, 42);
+        // $luckyNumbers = array(4, 8, 15, 16, 23, 42);
 
-        for ($i = 0; $i <= count($luckyNumbers); $i++) {
-            echo "$luckyNumbers[$i] <br>";
-        };
+        // for ($i = 0; $i <= count($luckyNumbers); $i++) {
+        //     echo "$luckyNumbers[$i] <br>";
+        // };
+    ?>
+
+    <!-- Including HTML in php -->
+    <?php
+        include "header.html";
+
+        include "footer.html";
     ?>
 
 
     <?php
-        $siteOwner = 'Patrick';
-        echo("<h1>$siteOwner's site</h1>");
-        echo "<hr>";
-        echo "<p>This is my site</p>";
+        // $siteOwner = 'Patrick';
+        // echo("<h1>$siteOwner's site</h1>");
+        // echo "<hr>";
+        // echo "<p>This is my site</p>";
 
-        echo "<h4>Working with string modifications</h4>";
-        echo strtolower($siteOwner);
-        echo "<br>";
-        echo strtoupper($siteOwner);
-        echo "<br>";
-        echo $siteOwner[0];
-        echo "<br>";
-        echo str_replace('Patrick', 'Abigayle', $siteOwner);
-        echo "<br>";
-        echo substr($siteOwner, 0, 3);
-        echo "<br>";
-        echo "<br>";
+        // echo "<h4>Working with string modifications</h4>";
+        // echo strtolower($siteOwner);
+        // echo "<br>";
+        // echo strtoupper($siteOwner);
+        // echo "<br>";
+        // echo $siteOwner[0];
+        // echo "<br>";
+        // echo str_replace('Patrick', 'Abigayle', $siteOwner);
+        // echo "<br>";
+        // echo substr($siteOwner, 0, 3);
+        // echo "<br>";
+        // echo "<br>";
 
 
         // Working with data types in php
         // Strings
-        $phrase = 'To be or not to be';
+        // $phrase = 'To be or not to be';
 
         // Numbers
-        $age = 30;
+        // $age = 30;
 
         // Floats
-        $gpa = 3.75;
+        // $gpa = 3.75;
 
         // Booleans
-        $isTrue = false;
+        // $isTrue = false;
 
         // Null
-        $nothingness = null;
+        // $nothingness = null;
 
         // Working with numbers in php
-        $num = 10;
-        echo '<h4>Working with numbers</h4>';
-        echo -47;
-        echo "<br>";
-        echo 5+9;
-        echo "<br>";
-        echo 5.79 * 31.9;
-        echo "<br>";
-        echo 10 % 3;
-        echo "<br>";
-        echo (4 + 5) * $num;
-        echo "<br>";
-        echo $num;
-        echo "<br>";
-        echo "<br>";        
+        // $num = 10;
+        // echo '<h4>Working with numbers</h4>';
+        // echo -47;
+        // echo "<br>";
+        // echo 5+9;
+        // echo "<br>";
+        // echo 5.79 * 31.9;
+        // echo "<br>";
+        // echo 10 % 3;
+        // echo "<br>";
+        // echo (4 + 5) * $num;
+        // echo "<br>";
+        // echo $num;
+        // echo "<br>";
+        // echo "<br>";        
 
 
         // $siteOwner = 'Not Patrick';
